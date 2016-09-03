86f487ba5649ab9c0fabfaaaf1f0c56b8ce86678
Fausto L <appmar23@gmail.com>
2016-09-03 19:33:49 -0400

    First Commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,1 @@
+<h1>The DashBoard</h1>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.master')
+
+@section('title')
+    Welcome!
+
+@endsection
+
+@section('content')
+    <div class="row">
+    
+    <!--Sign up -->
+    <h3>Sign Up</h3>
+        <div class="col-lg-6">
+            <form action="{{ route('signup') }}" method="post">
+                <div class="form-group">
+                    <label for="email">Your E-Mail</label>
+                    <input class="form-control" type="text" name="email" id="email">
+                </div>
+                <div class="form-group">
+                    <label for="first_name">Your First Name</label>
+                    <input class="form-control" type="text" name="first_name" id="first_name">
+                </div>
+                <div class="form-group">
+                    <label for="password">Your Password</label>
+                    <input class="form-control" type="password" name="password" id="password">
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+            </form>        
+        </div>
+         <div class="col-lg-6">
+
+         <!--Sign in -->
+         <h3>Sign In</h3>
+            <form action="{{  route('signin') }}" method="post">
+                <div class="form-group">
+                    <label for="email">Your E-Mail</label>
+                    <input class="form-control" type="text" name="email" id="email">
+                </div>                
+                <div class="form-group">
+                    <label for="password">Your Password</label>
+                    <input class="form-control" type="password" name="password" id="password">
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+                <input type="hidden" name="_token" value="{{ csrf_token() }}">
+            </form>
+    </div>
+
+@endsection
